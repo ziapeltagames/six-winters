91d890440f68f09170c6b9ad3b34487c39bd6543
pjlewis505 <gruefodder@gmail.com>
2018-04-21 11:16:16 -0600

    character and discord card iteration

--- src/timer-stats.py
+++ src/timer-stats.py
@@ -8,58 +8,86 @@ Created on Wed Mar  7 07:22:26 2018
 from random import shuffle
 import statistics
 
-progress_deck = ["timer,weather,team,common",
-                 ",safe,team,rare",
-                 ",suspicious,team,uncommon",
-                 ",darkness,team,uncommon",
-                 ",sorcery,team,common",
-                 ",doom,team,common",
-                 "timer,darkness,forceful,uncommon",
-                 ",weather,forceful,common",
-                 ",safe,forceful,common",
-                 ",darkness,forceful,common",
-                 ",doom,forceful,common",
-                 "timer,sorcery,quick,common",
-                 ",darkness,quick,uncommon",
-                 ",valor,quick,common",
-                 ",trap,quick,common",
-                 "timer,doom,sneaky,common",
-                 ",darkness,sneaky,uncommon",
-                 ",safe,sneaky,common",
-                 "timer,doom,weird,common",
-                 ",suspicious,weird,uncommon",
-                 ",doom,overwhelming,common",
-                 ",trap,deadly,rare",
-                 ",calamity,poisonous,common",
-                 "timer,suspicious,special,uncommon",
-                 "timer,blah,blah,blah",
-                 "timer,blah,blah,blah",
-                 "shuffle,,,"]
+#progress_deck = ["timer,weather,team,common",
+#                 ",safe,team,rare",
+#                 ",suspicious,team,uncommon",
+#                 ",darkness,team,uncommon",
+#                 ",sorcery,team,common",
+#                 ",doom,team,common",
+#                 "timer,darkness,forceful,uncommon",
+#                 ",weather,forceful,common",
+#                 ",safe,forceful,common",
+#                 ",darkness,forceful,common",
+#                 ",doom,forceful,common",
+#                 "timer,sorcery,quick,common",
+#                 ",darkness,quick,uncommon",
+#                 ",valor,quick,common",
+#                 ",trap,quick,common",
+#                 "timer,doom,sneaky,common",
+#                 ",darkness,sneaky,uncommon",
+#                 ",safe,sneaky,common",
+#                 "timer,doom,weird,common",
+#                 ",suspicious,weird,uncommon",
+#                 ",doom,overwhelming,common",
+#                 ",trap,deadly,rare",
+#                 ",calamity,poisonous,common",
+#                 "timer,suspicious,special,uncommon",
+#                 "shuffle,,,"]
+
+progress_deck = ["timer",
+                 "timer",
+                 "timer",
+                 "timer",
+                 "timer",
+                 "timer",
+                 "disaster",
+                 "disaster",
+                 "gang",
+                 "gang",
+                 "gang",
+                 "gang",
+                 "gang",
+                 "gang",
+                 "draw",
+                 "draw",
+                 "draw",
+                 "shuffle"]
 
 prd = list(progress_deck)
-avg_cards = []
 
-for i in range(0,100000):
+total_timers=0
+for card in progress_deck:
+    if "timer" in card:
+        total_timers = total_timers +1
+
+print('Total Timers',total_timers,' Deck Size',len(progress_deck))
+print('Percent Timers',total_timers/len(progress_deck))
+
+for num_timers in range(1,6):
     
-    shuffle(prd)
-    prd_ind = 0
-    cards = 0
-    timers = 0
+    avg_cards = []
     
-    while timers <= 5:
-    
-        if prd[prd_ind].find('timer') >= 0:
-            timers = timers + 1
+    for i in range(0,100000):
         
-        if prd[prd_ind].find('shuffle') >= 0:
-            shuffle(prd)
-            prd_ind = 0
-        else:
-            cards = cards + 1
-            prd_ind = prd_ind+1
+        shuffle(prd)
+        prd_ind = 0
+        cards = 0
+        timers = 0
+        
+        while timers < num_timers:
+        
+            if prd[prd_ind].find('timer') >= 0:
+                timers = timers + 1
             
-    avg_cards.append(cards)
+            if prd[prd_ind].find('shuffle') >= 0:
+                shuffle(prd)
+                prd_ind = 0
+            else:
+                cards = cards + 1
+                prd_ind = prd_ind+1
+                
+        avg_cards.append(cards)
 
-print(statistics.median(avg_cards))
-print(len(avg_cards))
-print(sum(avg_cards)/len(avg_cards))
+    print("Numer of Timers: ",num_timers)
+    print(statistics.median(avg_cards))
+    print(sum(avg_cards)/len(avg_cards))
