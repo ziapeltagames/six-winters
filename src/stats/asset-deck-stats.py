b0ebf81dbbbe119d1abfc8fd04af56e7dd6c32e6
pjlewis505 <gruefodder@gmail.com>
2019-02-10 16:17:52 -0700

    exploring number distribution

--- src/stats/asset-deck-stats.py
+++ src/stats/asset-deck-stats.py
@@ -6,6 +6,7 @@ Created on Fri Feb  8 04:28:35 2019
 """
 
 from random import shuffle
+from tqdm import tqdm
 import statistics
 import numpy as np
 
@@ -23,52 +24,58 @@ def build_deck():
     timer_deck = []
     timer_deck.append(lambda x: 0)
     timer_deck.append(lambda x: x - 2)
+    timer_deck.append(lambda x: x - 2)
     timer_deck.append(lambda x: x - 1)
-    timer_deck.append(lambda x: x - 1)
+    timer_deck.append(lambda x: x - 1)    
     timer_deck.append(lambda x: x)
     timer_deck.append(lambda x: x)
     timer_deck.append(lambda x: x)
+    timer_deck.append(lambda x: x + 1)    
     timer_deck.append(lambda x: x + 1)
-    timer_deck.append(lambda x: x + 1)
+    timer_deck.append(lambda x: x + 2)
     timer_deck.append(lambda x: 2 * x)
     
     timer_deck.append(lambda x: 0)
     timer_deck.append(lambda x: x - 2)
+    timer_deck.append(lambda x: x - 2)
     timer_deck.append(lambda x: x - 1)
-    timer_deck.append(lambda x: x - 1)
+    timer_deck.append(lambda x: x - 1)    
     timer_deck.append(lambda x: x)
     timer_deck.append(lambda x: x)
     timer_deck.append(lambda x: x)
+    timer_deck.append(lambda x: x + 1)    
     timer_deck.append(lambda x: x + 1)
-    timer_deck.append(lambda x: x + 1)
+    timer_deck.append(lambda x: x + 2)
     timer_deck.append(lambda x: 2 * x)
     return timer_deck
     
-epochs = 10000
+epochs = 100000
 deck_size = len(build_deck())
 single_vals = [2, 3]
 group_val = np.sum(single_vals)
 
 single_draw = []
     
-for e in range(epochs):
+for e in tqdm(range(epochs)):
 
     timer_deck = build_deck()
     shuffle(timer_deck)
     single_draw.append(timer_deck[0](group_val))
     
 single_avg = sum(single_draw) / len(single_draw)
-print('Single ',single_avg,' ',statistics.stdev(single_draw))
+print('Single ',single_avg,' ')
+print('STD ',statistics.stdev(single_draw[:10000]))
 
 double_draw = []
     
-for e in range(epochs):
+for e in tqdm(range(epochs)):
 
     timer_deck = build_deck()
     shuffle(timer_deck)
     double_draw.append(timer_deck[0](single_vals[0]) + timer_deck[1](single_vals[1]))
     
 double_avg = sum(double_draw) / len(double_draw)
-print('Double ',double_avg,' ',statistics.stdev(double_draw))
+print('Double ',double_avg,' ')
+print('STD ',statistics.stdev(double_draw[:10000]))
 
 print('Difference ',single_avg - double_avg)
