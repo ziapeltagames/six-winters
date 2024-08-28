72ab8a6a8c15811579d28a011c4d6749d94fa313
phil <gruefodder@gmail.com>
2024-08-27 18:04:35 -0600

    removed consequences from scenes

--- src/generate/sort_cards_by_deck.py
+++ src/generate/sort_cards_by_deck.py
@@ -6,142 +6,28 @@ import os
 
 for subloc in ["tts", "game_crafter"]:
 
-    ob_mission =  [1, 200]
+    indices =  [1, 200]
 
-    obpath = '..\\Cards\\'+subloc+'\\obstacles\\'
+    for subpath in ["obstacles", "events", "text_obstacles", "assets", "abilities", "locations", "scenes", "schemes", "stats"]:
 
-    for i in range(ob_mission[0], ob_mission[1], 2):
-        if i == 1:
-            obs = glob.glob(obpath+'*-1.png')
-        else:
-            obs = glob.glob(obpath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
+        full_path = '..\\Cards\\'+subloc+'\\'+subpath+'\\'
+
+        for i in range(indices[0], indices[1], 2):
             if i == 1:
-                cardnum = '2'
+                files = glob.glob(full_path+'*-1.png')
             else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], obpath+'front\\'+base+'-1'+cardnum+'[face].png')
+                files = glob.glob(full_path+'*-1'+str(i)+'.png')
+            if files:
+                base, cardnum = os.path.basename(files[0]).split('-')
+                if i == 1:
+                    cardnum = '2'
+                else:
+                    cardnum = str(int(cardnum[1:-4]) + 1)
+                move(files[0], full_path+'front\\'+base+'-1'+cardnum+'[face].png')
 
-        obs = glob.glob(obpath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], obpath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
-
-
-for subloc in ["tts", "game_crafter"]:
-
-    square_progs =  [1, 200]
-    square_progpath = '..\\Cards\\'+subloc+'\\assets\\'
-
-    for i in range(square_progs[0], square_progs[1], 2):
-        if i == 1:
-            obs = glob.glob(square_progpath+'*-1.png')
-        else:
-            obs = glob.glob(square_progpath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
-            if i == 1:
-                cardnum = '2'
-            else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], square_progpath+'front\\'+base+'-1'+cardnum+'[face].png')
-
-        obs = glob.glob(square_progpath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], square_progpath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
-
-for subloc in ["tts", "game_crafter"]:
-
-    locs =  [1, 120]
-
-    locpath = '..\\Cards\\'+subloc+'\\locations\\'
-
-    for i in range(locs[0], locs[1], 2):
-        if i == 1:
-            obs = glob.glob(locpath+'*-1.png')
-        else:
-            obs = glob.glob(locpath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
-            if i == 1:
-                cardnum = '2'
-            else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], locpath+'front\\'+base+'-1'+cardnum+'[face].png')
-
-        obs = glob.glob(locpath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], locpath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
-
-
-for subloc in ["tts", "game_crafter"]:
-
-    miss =  [1, 99]
-
-    misspath = '..\\Cards\\'+subloc+'\\missions\\'
-
-    for i in range(miss[0], miss[1], 2):
-        if i == 1:
-            obs = glob.glob(misspath+'*-1.png')
-        else:
-            obs = glob.glob(misspath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
-            if i == 1:
-                cardnum = '2'
-            else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], misspath+'front\\'+base+'-1'+cardnum+'[face].png')
-
-        obs = glob.glob(misspath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], misspath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
-
-for subloc in ["tts", "game_crafter"]:
-
-    scene =  [1, 99]
-
-    scenepath = '..\\Cards\\'+subloc+'\\scenes\\'
-
-    for i in range(scene[0], scene[1], 2):
-        if i == 1:
-            obs = glob.glob(scenepath+'*-1.png')
-        else:
-            obs = glob.glob(scenepath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
-            if i == 1:
-                cardnum = '2'
-            else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], scenepath+'front\\'+base+'-1'+cardnum+'[face].png')
-
-        obs = glob.glob(scenepath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], scenepath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
-
-for subloc in ["tts", "game_crafter"]:
-
-    achieve =  [1, 99]
-
-    achievepath = '..\\Cards\\'+subloc+'\\achievements\\'
-
-    for i in range(achieve[0], achieve[1], 2):
-        if i == 1:
-            obs = glob.glob(achievepath+'*-1.png')
-        else:
-            obs = glob.glob(achievepath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
-            if i == 1:
-                cardnum = '2'
-            else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], achievepath+'front\\'+base+'-1'+cardnum+'[face].png')
-
-        obs = glob.glob(achievepath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], achievepath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
+            files = glob.glob(full_path+'*-1'+str(i+1)+'.png')
+            if files:
+                move(files[0], full_path+'back\\'+os.path.basename(files[0])[:-4]+'[back].png')
 
 
 chars =  [1, 99]
@@ -164,27 +50,6 @@ for i in range(chars[0], chars[1], 2):
     obs = glob.glob(charpath+'*-1'+str(i+1)+'.png')
     if obs:
         move(obs[0], charpath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
-
-for subloc in ["tts", "game_crafter"]:
-
-    charpath = '..\\Characters\\'+subloc+'\\mats\\'
-
-    for i in range(chars[0], chars[1], 2):
-        if i == 1:
-            obs = glob.glob(charpath+'*-1.png')
-        else:
-            obs = glob.glob(charpath+'*-1'+str(i)+'.png')
-        if obs:
-            base, cardnum = os.path.basename(obs[0]).split('-')
-            if i == 1:
-                cardnum = '2'
-            else:
-                cardnum = str(int(cardnum[1:-4]) + 1)
-            move(obs[0], charpath+'front\\'+base+'-1'+cardnum+'[face].png')
-
-        obs = glob.glob(charpath+'*-1'+str(i+1)+'.png')
-        if obs:
-            move(obs[0], charpath+'back\\'+os.path.basename(obs[0])[:-4]+'[back].png')
 
 rulepath = '..\\Rules\\game_crafter\\'
 
--- src/generate/stable_diffusion_generator.py
+++ src/generate/stable_diffusion_generator.py
@@ -18,188 +18,47 @@ artists = ["wayne reynolds", "john stanko", "kieran yanner", "artgerm", "greg ru
 # edmund dulac: muted tones
 # michael garmash: heavy brush strokes, more abstract
 
-loc_prompts = []
-    
-old_loc_prompts = ["painting of fantasy persian city on the sea, with many houses and markets, boats are in the harbor, sun is setting, the beaches are crimson, for dungeons and dragons  ",
-"painting of a cloaked fantasy figure, walking through the snow, between tall rocky mountains, setting sun, stars coming out above ",
-"painting of four seasons, swirled together in a circle, mixing ",
-"painting of travelers in the distance, walking through a snowy mountain pass, tall mountains surround them ",
-"painting of fantasy persian city on the sea, boats are in the harbor, sun is setting, the beaches are crimson, for dungeons and dragons  ",
-"painting of will o wisps in a swamp, the water is glowing orange and blue, vines hang above the water, for dungeons and dragons ",
-"painting of a fantasy throne room, for dungeons and dragons, with pale lavender lighting, high brass arches ",
-"painting of ghosts floating through dense forest, with purple clouds floating overhead, for dungeons and dragons ",
-"painting of a magical forest, with translucent purple ghost trees and dark gray rivers, for dungeons and dragons ",
-"painting of byzantine throne room, with brass throne, and red carpets, pale lavender lighting ",
-"painting of a ruined fantasy city, crumbling walls, crows flying overhead, the sun is setting, for dungeons and dragons ",
-"painting of magical wizard's tower, rising above celtic village, with stars in the sky ",
-"painting of wizard's tower, rising above celtic village, with hills in the background ",
-"painting of long persian hall, with brass doors, and brass statues on either side of a lavender carpet ",
-"painting of long hall in persian castle, with brass doors both sides ",
-"painting of desolate plains in mongolia, with rain clouds overhead ",
-"painting of an alley in a fantasy city for dungeons and dragons, rain clouds are overhead, the houses are dreary ",
-"painting of a slavic village, with a stream running through it, deep in the forest",
-"painting of a large wooden fantasy building, long and narrow, in a mountain valley",
-"painting of a long fantasy hall, full of mirrors, and sunlight coming through windows high in the ceiling ",
-"painting of a fantasy cottage, built into the top of a large tree, in a dense forest ",
-"painting of a fantasy persian house, built into a large forest, with a stream running past ",
-"painting of fantasy tavern, with lavender smoke swirling around the customers ",
-"painting of a fantasy opium den, with lavender pillows and a large brass hookah, with lavender smoke swirling around ",
-"painting of rocky columns forming an eldritch helix, standing on a hill ",
-"painting of fields of wheat and barley in an ancient valley, with farm houses in the distance ",
-"painting of ancient roman farms, with hills in the backgrounds ",
-"painting of small valley with a narrow stream running through it, trees on both sides of the stream, leaves are over the stream in the style of ",
-"painting of a green field with rocky monuments for dungeons and dragons ",
-"painting of fantasy female roman peasent standing next to cart filled with squid and octopus ",
-"painting of fantasy fish merchant stall, selling squid and octopus ",
-"painting of dark forest with white and gray trees, colorful mushrooms grow on the ground ",
-"painting of fantasy fishmonger, with cart carrying squid and octopus, for dungeons and dragons ",
-"painting of fantasy persian village with trading carts and amaranth, for dungeons and dragons ",
-"painting of fantasy military fort with white walls in the hills for dungeons and dragons ",
-"painting of fantasy dungeon hall with many jail cells, dim lighting from torches, for dungeons and dragons ",
-"painting of fantasy prison with many jail cells, for dungeons and dragons ",
-"painting of medieval torture devices, for dungeons and dragons ",
-"painting of roman fort in the hills, for dungeons and dragons ",
-"painting of dimly lit, crowded fantasy tavern, for dungeons and dragons ",
-"painting of persian castle made of brass in the distance with a field of amaranth, for dungeons and dragons ",
-"painting of glowing blue forest, dense trees, ghosts, for dungeons and dragons ",
-"painting of stone statues of skulls and tentacles on a rocky mountain, for dungeons and dragons ",
-"painting of the inside of a fantasy roman fort, in a mountain pass, for dungeons and dragons ",
-"painting of roman fort with an open portcullis, sitting in a mountain valley, for dungeons and dragons "
-"painting of roman generals walking in dimly lit hall with marble columns, for dungeons and dragons ",
-"painting of roman generals standing around table with maps on it, dimly lit room, for dungeons and dragons ",
-"painting of dungeons and dragons fighters walking down hall with torches in the walls, for dungeons and dragons ",
-"painting of fantasy castle wall with catapults in front of it, for dungeons and dragons ",
-"painting of mongolian trading house, in wide open field, for dungeons and dragons ",
-"painting of fantasy spooky dark forest, for dungeons and dragons ",
-"painting of roman military hall, with battle maps, for dungeons and dragons ",
-"painting of fantasy tavern, with lavender smoke filling the air, for dungeons and dragons ",
-"painting of fantasy dungeon with torture chamber, for dungeons and dragons ",
-"painting of roman fantasy dungeon with cell doors, for dungeons and dragons ",
-"painting of fantasy roman castle in the mountains, for dungeons and dragons ",
-"painting of fantasy roman city bustling with trade, for dungeons and dragons ",
-"painting of old keep, stone walls, in mountain valley, for dungeons and dragons ",
-"painting of fantasy keep in the desert, for dungeons and dragons ",
-"painting of fantasy persian brass palace in the desert, for dungeons and dragons ",
-"painting of fantasy byzantine port city with boats and brass buildings, for dungeons and dragons ",
-"painting of fantasy persian market square, with trading carts, for dungones and dragons ",
-"painting of smoke filled room with brass statues, for dungeons and dragons ",
-"painting of gloomy caves, glowing with energy, for dungeons and dragons ",
-"painting of fantasy library, filled with old books, dimly lit, for dungeons and dragons ",
-"painting of wide open green field with mountains in the background, for dungeons and dragons ",
-"painting of tall forbidding mountains, for dungeons and dragons "
-]
-
-obs_prompts = ["painting of magical mist draining the life force from fantasy villagers",
-               "painting of a magical grey glowing cloud in a fantasy persian village",
-               "painting of a fantasy spell from dungeons and dragons",
-               "magical spell dungeons and dragons blasting death magic",
-               "eldritch spell energy dark red clouds swirling magic",
-               "assassin wearing a blue cloak with a scar for dungeons and dragons",
-               "a charming bard with an evil grin and a lute for dungeons and dragons",
-               "fantasy assassin in a city for conan"]
-
-old_obs_prompts = [
-"painting of a fantasy ranger in a cloak sitting on a rock smoking a pipe with gray smoke rings coming out of it ",
-"drawing of a pipe on a table, sitting next to a blue and white poweder ",
-"drawing of a fantasy traveler, sitting in a dimly lit tavern, smoking a pipe with stars and moons flying out of it ",
-"painting of dimly lit, crowded fantasy tavern, for dungeons and dragons ",
-"painting of a fat mongolian merchant in fine clothes standing next to a tavern, for dungeons and dragons ",
-"painting of a colorful magical tome, glowing with magical energy, for dungeons and dragons ",
-"painting of a female rogue wearing a cloak, sneaking into a dark vault with a torch on the wall and a table with books on it, for dungeons and dragons ",
-"painting of glowing blue magical energy swirling above a persian village, for dungeons and dragons "
-]
-
 negative_prompt = "picture frame, poorly drawn, ugly, tiling, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy, writing, calligraphy, sign, cut off, cartoon, vector art, clipart, distorted, amateur, split"
 
-odl_ast_prompts = ["painting of bread in medieval tavern",
-                    "painting of grapes in medieval tavern",
-                    "painting of mug of ale in medieval tavern",
-                    "painting of fantasy scarf with persian design",
-                    "painting of fantasy spell turning lead into gold",
-                    "painting of fantasy spell polymorph other for dungeons and dragons",
-                    "painting of medieval cannons for dungeons and dragons",
-                    "painting of fantasy war machine for dungeons and dragons",
-                    "painting of fantasy medieval love letter",
-                    "painting of mysterious message using magical ink",
-                    "painting of scribe hireling for dugeons and dragons",
-                    "painting of torchbearer hireling for dungeons and dragons",
-                    "painting of sharp fantasy sword",
-                    "painting of large fantasy crowssbow for dungeons and dragons",
-                    "painting of massive fantasy crossbow firing magical bolt",
-                    "painting of magical potion for dungeons and dragons",
-                    "painting of fantasy costume ball mask for dungeons and dragons",
-                    "painting of fantasy costume ball dress for dungeons and dragons",
-                    "painting of fantasy armor for dungeons and dragons",
-                    "painting of fantasy mongolian armor",
-                    "painting of fantasy roman armor",
-                    "painting of bubbling cauldron for dungeons and dragons",
-                    "painting of wineskin for dungeons and dragons",
-                    "painting of flint and steel for dungeons and dragons",
-                    "painting of lyre sitting on table for dungeons and dragons",
-                    "painting of fantasy drums",
-                    "painting of rope for dungeons and dragons",
-                    "painting of burlap sack for dungeons and dragons",
-                    "painting of fantasy tent for dungeons and dragons",
-                    "painting of crossbow sitting on table for dungeons and dragons",
-                    "painting of scarf for dungeons and dragons",
-                    "painting of cat familiar for dungeons and dragons",
-                    "painting of turtle familiar for dungeons and dragons",
-                    "painting of dog companion for dungeons and dragons",
-                    "painting of fantasy dog",
-                    "painting of fantasy dragonfly for dungeons and dragons",
-                    "painting of arrows sitting on a medieval table",
-                    "painting of a fantasy longbow sitting against a forest tree",
-                    "painting of a fantasy grappling hook for dungeons and dragons",
-                    "painting of fantasy climbing spikes for dungeons and dragons",
-                    "painting of an apple on a medieval table",
-                    "painting of a wizard walking into a portal for dungeons and dragons",
-                    "painting of an arcane trickster walking to a magical portal for dungeons and dragons"]
+loc_prompts = ["painting of golden hills with small stream and magical forest for dungeons and dragons",
+               "painting of golden hills with large mountains behind them for dungeons and dragons"]
 
-ast_prompts = ["fantasy dungeons and dragons magical wand of mirrors",
-               "fantasy dungeons and dragons magic item mirrors of heat",
-               "painting of fantasy mirror weapon",
-               "fantasy painting dungeons and dragons mirror artifact"]
+obs_prompts = ["fantasy painting of twisted tree roots oozing a black deathly substance",
+               "fantasy painting of twisted bloody roots an evil ball",
+               "fantasy painting of a white dragon staring coldly ahead",
+               "fantasy painting of a tawny dragon with a laughing expression",
+               "fantasy painting for dungeons and dragons of fantasy merchants yelling at each other"]
+
+ast_prompts = ["a corporate icon of an ankylosaurus with a 20 sided die for a tail",
+               "a graphic design of an ankylosaurus with a d20 on its tail",
+               "a corporate logo featuring a brown ankylosaurus dinosaur",
+               "a dinosaur logo with a d20 for a body",
+               "a fantasy dinosaur logo with a 20 sided die for a body"]
 
 back_prompts = ["byzantine mosaic of lavender and crimson",
                 "byzantine mosaic",
                 "persian pattern from sasanian empire",
                 "persian pattern"]
 
-prompts = back_prompts
-
-# Standard: 768px x 768px
+prompts = ast_prompts
 
 # Generating imagery for 240dpi, a little lower than the normal 300dpi, but still looks okay
-
-# Trade board at 240dpi: 18.25in x 18.25in (4380px x 4380px)
-# height = 768
-# width = 768
 
 # Obstacle imagery at 300dpi: 2.25in x 2.093in (675px x 628px)
 # height = 624
 # width = 672
 
-# Location imagery at 240dpi: 3.83in x 2.154in (920px x 520px)
-# height = 520
-# width = 920
-
-# Large location imagery at 300dpi: 5.75 x 3.06 (1725px x 920px)
-# Large location imagery at 240dpi: 1380px x 734px
-# Asset imagery at 300dpi: 2.75 in x 1.4 in (825px x 420px)
-# width = 832
-# height = 424
+# Location imagery at 240dpi: 5.75 x 3.0583
+# width = 1384
+# height = 736
 
 # Changed asset to 2.75 in x 1.76 in (825px x 528px)
-# width = 832
-# height = 528
+width = 832
+height = 528
 
 # Card backgrounds 2.75 in x 3.75 in
-width = 832
-height = 1128
-
-# Square asset, must be cropped
-# height = 592
 # width = 832
+# height = 1128
 
 for i in range(200):
 
